d9ee31031f4a0ae34a50e7ddb6ac88b321c431ad
Zephyr Mann <zephmann@gmail.com>
2024-12-06 18:13:16 -0500

    day 2, star 2

--- src/advent/day_02.py
+++ src/advent/day_02.py
@@ -13,6 +13,28 @@ def total_safe_reports(reports: list[list[int]]) -> int:
     return len([r for r in reports if _is_report_safe(r)])
 
 
+def total_safe_reports_with_dampener(reports: list[list[int]]) -> int:
+    """Determine the total number of safe reports with "problem dampener".
+
+    Now, the same rules apply as before, except if removing a single level
+    from an unsafe report would make it safe, the report instead counts as safe.
+    """
+    total = 0
+    for report in reports:
+        if _is_report_safe(report):
+            total += 1
+            continue
+
+        # TODO could be better
+        for i in range(len(report)):
+            sub_report = report[:i] + report[i + 1 :]
+            if _is_report_safe(sub_report):
+                total += 1
+                break
+
+    return total
+
+
 def _is_report_safe(report):
     if report[0] < report[1]:
         report.reverse()
@@ -38,6 +60,18 @@ def test_total_safe_reports():
     assert total_safe_reports(values) == 2
 
 
+def test_total_safe_reports_with_dampener():
+    values = [
+        [7, 6, 4, 2, 1],
+        [1, 2, 7, 8, 9],
+        [9, 7, 6, 2, 1],
+        [1, 3, 2, 4, 5],
+        [8, 6, 4, 4, 1],
+        [1, 3, 6, 7, 9],
+    ]
+    assert total_safe_reports_with_dampener(values) == 4
+
+
 @pytest.mark.parametrize(
     "report,is_safe",
     (
@@ -57,3 +91,4 @@ if __name__ == "__main__":
     with open(advent.get_input_file(__file__)) as f:
         values = [[int(y) for y in x.split()] for x in f.readlines() if x]
         print(f"Part 1: {total_safe_reports(values)}")
+        print(f"Part 2: {total_safe_reports_with_dampener(values)}")
